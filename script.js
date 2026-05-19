19c28b3117e40b86682065bec55fbb438f2f9814
zenqwe1112-cmd <zenqwe1112@gmail.com>
2026-05-19 21:52:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let questions = [
+"ชอบดู YouTube แบบไหน?",
+"ชอบสีอะไร?",
+"ชอบเที่ยวไหม?"
+];
 
+let current = 0;
+
+function next(answer){
+current++;
+
+if(current < questions.length){
+document.getElementById("question").innerText = questions[current];
+document.getElementById("count").innerText =
+(current+1)+"/"+questions.length;
+}
+else{
+document.querySelector(".card").innerHTML =
+"<h2>คุณเป็นสายชิล 😆</h2>";
+}
+}
